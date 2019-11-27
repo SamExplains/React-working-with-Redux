fe44a6ecf5bfc832af0b509af435b00849fc432a
Sampuccino <Sampuccino@gmail.com>
2019-11-27 10:15:55 -0800

    Connected the Provider and Redux store to main App component.

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    {title: 'First Song', duration: '3:00'},
+    {title: 'Second Song', duration: '3:15'},
+    {title: 'Third Song', duration: '4:45'},
+    {title: 'Fourth Song', duration: '2:45'},
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selected: selectedSongReducer
+});
